ff31df2b1c8db9780ed95f438ce8bdf0df87e0ae
Tuoi-SE <tuoi.22774801030222@vanlanguni.vn>
2025-12-16 16:23:03 +0700

    Video 18 Using MongoDB with NestJS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('mongodb+srv://funny_tuoi:cB0B4B2gimZpOBPe@nestjs.rmitfzy.mongodb.net/?appName=NestJS')],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
