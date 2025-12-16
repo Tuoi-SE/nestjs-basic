765e299df6e315c96e22e0ef9e10d6e8c1d23881
Tuoi-SE <tuoi.22774801030222@vanlanguni.vn>
2025-12-16 16:48:33 +0700

    Video 19 ENV Variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://funny_tuoi:cB0B4B2gimZpOBPe@nestjs.rmitfzy.mongodb.net/?appName=NestJS')],
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
