30db9ac2f9ff1f5fee26f78618d5ee15836e4b2f
Tuoi-SE <tuoi.22774801030222@vanlanguni.vn>
2025-12-17 15:51:25 +0700

    Video 22(Part 5) Validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,20 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 //data transfer object // class = {}
 export class CreateUserDto {
+    @IsEmail({}, {
+        message: 'Email không đúng định dạng',
+    })
+    @IsNotEmpty({
+        message: 'Email không được để trống',
+    })
     email: string;
+
+    @IsNotEmpty({
+        message: 'Password không được để trống',
+    })
     password: string;
+
     name: string;
     address: string;
 }
