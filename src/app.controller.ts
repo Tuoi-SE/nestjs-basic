946f9842effb6782f9b8e3778762927958d96570
Tuoi-SE <tuoi.22774801030222@vanlanguni.vn>
2025-12-19 10:24:53 +0700

    Video 42 Disable Guard

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,7 @@ import { ConfigService } from '@nestjs/config';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { Public } from './decorator/customize';
 
 @Controller()
 export class AppController {
@@ -12,6 +13,7 @@ export class AppController {
     private readonly configService: ConfigService,
     private authService: AuthService) { }
 
+  @Public()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
