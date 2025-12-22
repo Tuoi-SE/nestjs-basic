795e0f2bc22c3c31924bf4aaa54f38c43f946a6b
Tuoi-SE <tuoi.22774801030222@vanlanguni.vn>
2025-12-22 10:28:05 +0700

    Video 55 Update User Type (JWT)

--- src/auth/passport/local.strategy.ts
+++ src/auth/passport/local.strategy.ts
@@ -11,10 +11,10 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(username: string, password: string): Promise<any> {
-        const user = await this.authService.validateUser(username, password);
-        if (!user) {
-            throw new UnauthorizedException();
+        const user = await this.authService.validateUser(username, password); if
+            (!user) {
+            throw new UnauthorizedException("Invalid Username/Password !");
         }
-        return user;
+        return user; //req.user
     }
 }
