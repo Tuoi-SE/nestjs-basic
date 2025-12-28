5c0e7ddda169e376d5cb597b00ff0bdd7ad2a1d2
Tuoi-SE <tuoi.2274801030222@vanlanguni.vn>
2025-12-29 00:05:50 +0700

    Video 71(Part 2) Fix CORS for FE

--- src/main.ts
+++ src/main.ts
@@ -28,9 +28,10 @@ async function bootstrap() {
 
   // Config CORS
   app.enableCors({
-    "origin": "*",
+    "origin": true,
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
+    credentials: true
   });
 
   // Config Version
