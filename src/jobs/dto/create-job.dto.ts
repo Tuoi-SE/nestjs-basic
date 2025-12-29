35041e01450cdf2bba81e634a70d9cbceb85081e
Tuoi-SE <tuoi.2274801030222@vanlanguni.vn>
2025-12-29 11:03:34 +0700

    Video 72 Create Module Jobs

---
+++ src/jobs/dto/create-job.dto.ts
@@ -0,0 +1,45 @@
+import { Type } from "class-transformer";
+import { IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from "class-validator";
+import mongoose from "mongoose";
+
+class Company {
+    @IsNotEmpty()
+    _id: mongoose.Schema.Types.ObjectId;
+
+    @IsNotEmpty()
+    name: string;
+}
+
+export class CreateJobDto {
+    @IsNotEmpty({ message: 'Name không được để trống', })
+    name: string;
+
+    @IsNotEmpty({ message: 'Skill không được để trống', })
+    skills: string[];
+
+    @IsNotEmptyObject()
+    @IsObject()
+    @ValidateNested()
+    @Type(() => Company)
+    company: Company;
+
+    @IsNotEmpty({ message: 'Salary không được để trống', })
+    salary: number;
+
+    @IsNotEmpty({ message: 'Quantity không được để trống', })
+    quantity: number;
+
+    @IsNotEmpty({ message: 'Level không được để trống', })
+    level: string;
+
+    description: string;
+
+    @IsNotEmpty({ message: 'StartDate không được để trống', })
+    startDate: Date;
+
+    @IsNotEmpty({ message: 'EndDate không được để trống', })
+    endDate: Date;
+
+    @IsNotEmpty({ message: 'IsActive không được để trống', })
+    isActive: boolean;
+}
