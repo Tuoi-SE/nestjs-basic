7eb54bbb2df24e7d34b49645a7e637c42f0c3be7
Tuoi-SE <tuoi.2274801030222@vanlanguni.vn>
2025-12-31 19:07:41 +0700

    Video 74 Fix CRUD Jobs

--- src/jobs/dto/create-job.dto.ts
+++ src/jobs/dto/create-job.dto.ts
@@ -1,5 +1,5 @@
-import { Type } from "class-transformer";
-import { IsNotEmpty, IsNotEmptyObject, IsObject, ValidateNested } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import { IsArray, IsBoolean, IsDate, IsNotEmpty, IsNotEmptyObject, IsObject, IsString, ValidateNested } from "class-validator";
 import mongoose from "mongoose";
 
 class Company {
@@ -15,6 +15,9 @@ export class CreateJobDto {
     name: string;
 
     @IsNotEmpty({ message: 'Skill không được để trống', })
+    @IsArray({ message: "Skill có định dạng là array" })
+    //each: true kiểm tra từng phần tử trong array có phải là string không
+    @IsString({ each: true, message: "Skill định dạng là String" })
     skills: string[];
 
     @IsNotEmptyObject()
@@ -32,14 +35,20 @@ export class CreateJobDto {
     @IsNotEmpty({ message: 'Level không được để trống', })
     level: string;
 
+    @IsNotEmpty({ message: 'Description không được để trống', })
     description: string;
 
+    @IsDate({ message: "startDate có định dạng là Date" })
+    @Transform(({ value }) => new Date(value))
     @IsNotEmpty({ message: 'StartDate không được để trống', })
     startDate: Date;
 
+    @IsDate({ message: "endDate có định dạng là Date" })
+    @Transform(({ value }) => new Date(value))
     @IsNotEmpty({ message: 'EndDate không được để trống', })
     endDate: Date;
 
     @IsNotEmpty({ message: 'IsActive không được để trống', })
+    @IsBoolean({message: "isActive có định dạng là boolean"})
     isActive: boolean;
 }
