5a10c090f3dbf394d48c9f24e7202dda280f2751
Tuoi-SE <tuoi.2274801030222@vanlanguni.vn>
2025-12-31 18:41:33 +0700

    Video 73 Assignment CRUD functions jobs

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,34 +1,45 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { IUser } from 'src/users/users.interface';
+import { ResponseMessage, User } from 'src/decorator/customize';
 
 @Controller('jobs')
 export class JobsController {
-  constructor(private readonly jobsService: JobsService) {}
+  constructor(private readonly jobsService: JobsService) { }
 
+  @ResponseMessage("Create a new Job")
   @Post()
-  create(@Body() createJobDto: CreateJobDto) {
-    return this.jobsService.create(createJobDto);
+  create(@Body() createJobDto: CreateJobDto, @User() user: IUser) {
+    return this.jobsService.create(createJobDto, user);
   }
 
+  @ResponseMessage("Fetch jobs with pagination")
   @Get()
-  findAll() {
-    return this.jobsService.findAll();
+  findAll(
+    @Query("current") currentPage: string,
+    @Query("pageSize") limit: string,
+    @Query() qs: string
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, qs);
   }
 
+  @ResponseMessage("Fetch a job by id")
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.jobsService.findOne(+id);
+    return this.jobsService.findOne(id);
   }
 
+  @ResponseMessage("Update a job")
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto) {
-    return this.jobsService.update(+id, updateJobDto);
+  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto, @User() user: IUser) {
+    return this.jobsService.update(id, updateJobDto, user);
   }
 
+  @ResponseMessage("Delete a job")
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.jobsService.remove(+id);
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.jobsService.remove(id, user);
   }
 }
